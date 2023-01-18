8b45e797f4ee3bc99f52dfa58f9dafd4b967f197
code-with-santanu <santanupal003@gmail.com>
2023-01-18 19:18:48 +0530

    function declarion added

--- SearchingAlgo.c
+++ SearchingAlgo.c
@@ -1,6 +1,47 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void InsertInArray(int arr[],int );
+void Print(int arr[],int );
+int LinearSearch(int arr[],int ,int );
+int BinarySearch(int arr[],int ,int );
+int InterpolationSearch(int arr[],int , int );
+
+
+int main()
+{
+    int *a;
+    int size,targetNo,searchIndex;
+
+    printf("\nEnter the size of the array: ");
+    scanf("%d",&size);
+    a=(int*)malloc(sizeof(int)*size);
+
+    InsertInArray(a,size);
+    Print(a,size);
+    printf("\nEnter a no from the array to search: ");
+    scanf("%d",&targetNo);
+
+
+    //choose any one algo and comment rest to algo before run the program
+    searchIndex=LinearSearch(a,size,targetNo);        
+    searchIndex=BinarySearch(a,size,targetNo);
+    searchIndex=InterpolationSearch(a,size,targetNo);
+
+    if(searchIndex==-1)
+    {
+        printf("\nThe no(%d) you are searching is not in the array.",targetNo);
+    }
+    else
+    {
+        printf("\nThe no(%d) you are searching is at %d th position in the array.",targetNo,searchIndex);
+    }
+
+    return 0;
+
+}
+
+
 void InsertInArray(int arr[],int size)
 {
     int i,n;
@@ -108,39 +149,4 @@ int InterpolationSearch(int arr[],int size, int targetNo)
     }
     
     return -1;
-}
-
-
-
-int main()
-{
-    int *a;
-    int size,targetNo,searchIndex;
-
-    printf("\nEnter the size of the array: ");
-    scanf("%d",&size);
-    a=(int*)malloc(sizeof(int)*size);
-
-    InsertInArray(a,size);
-    Print(a,size);
-    printf("\nEnter a no from the array to search: ");
-    scanf("%d",&targetNo);
-
-
-    //choose any one algo and comment rest to algo before run the program
-    searchIndex=LinearSearch(a,size,targetNo);        
-    searchIndex=BinarySearch(a,size,targetNo);
-    searchIndex=InterpolationSearch(a,size,targetNo);
-
-    if(searchIndex==-1)
-    {
-        printf("\nThe no(%d) you are searching is not in the array.",targetNo);
-    }
-    else
-    {
-        printf("\nThe no(%d) you are searching is at %d th position in the array.",targetNo,searchIndex);
-    }
-
-    return 0;
-
 }
